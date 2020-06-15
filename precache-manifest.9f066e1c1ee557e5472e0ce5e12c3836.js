47f69363d79eead391adba0cccde61dc913ee40e
Patrik Hägg <patrik_hagg@hotmail.com>
2020-06-15 11:04:35 +0200

    Updates

---
+++ precache-manifest.9f066e1c1ee557e5472e0ce5e12c3836.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c7f32bab0cbd124baa0256b6b42c4b4",
+    "url": "/phagg/monsters/index.html"
+  },
+  {
+    "revision": "0928a7c66032c4d6b427",
+    "url": "/phagg/monsters/static/css/main.0561c428.chunk.css"
+  },
+  {
+    "revision": "299f65059b0504546074",
+    "url": "/phagg/monsters/static/js/2.eca8dab0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/phagg/monsters/static/js/2.eca8dab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0928a7c66032c4d6b427",
+    "url": "/phagg/monsters/static/js/main.c23b3b5d.chunk.js"
+  },
+  {
+    "revision": "6d93a7751e7a4b81cbf8",
+    "url": "/phagg/monsters/static/js/runtime-main.41086fab.js"
+  }
+]);
